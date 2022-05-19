1b6ac5239da4ca5ba234d406cbd6836e2a312282
chrisg.local@gmail.com <chrisg.local@gmail.com>
2022-05-19 21:15:53 +1000

    New Round Box Add new method to round corners on a box using minkowski

--- Basic Geometry/game_insert.py
+++ Basic Geometry/game_insert.py
@@ -3,9 +3,10 @@ import sys
 
 from solid import scad_render_to_file
 from solid.objects import cube, cylinder, difference, translate, union
-from solid.utils import right,left,up,down
+from solid.utils import right,left,up,down,minkowski
 
 SEGMENTS = 48
+
 def game_insert():
 
     length, width, height = 100,150,50
@@ -13,13 +14,27 @@ def game_insert():
     wall = 5
     double_wall = wall*2
 
-
     box = (cube([length,width,height]))
     inner_box = (cube([length-double_wall,width-double_wall,height]))
     # Offset the Inner Box
     inner_box = translate([wall,wall,wall])(inner_box)
+    insert = roundbox([length-double_wall,width-double_wall,height],2)
+    # Offset the insert
+    insert = translate([wall,wall,wall])(insert)
+    return box - insert
 
-    return box - inner_box
+def roundbox(size, radius):
+    """"box with rounded edges."""
+
+    x,y,z = size
+    x = x - radius * 2
+    y = y - radius * 2
+    z = z - 1
+
+    round_box = minkowski()(cube([x,y,z]))(cylinder(r=radius))
+    round_box = translate([radius,radius,0])(round_box)
+
+    return round_box
 
 if __name__ == '__main__':
     out_dir = sys.argv[1] if len(sys.argv) > 1 else None
