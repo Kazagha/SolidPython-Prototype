4ec791e7a56f60c4a0b38a5a2087497d42c372b7
chrisg.local@gmail.com <chrisg.local@gmail.com>
2022-06-21 13:14:52 +1000

    Hex_Wall: Clean Up Clean up the code and add more comments

--- Basic Geometry/Hex_Wall.py
+++ Basic Geometry/Hex_Wall.py
@@ -18,6 +18,7 @@ def Create_Hex(radius, wall_height, hex_wall):
     return hex
 
 def Hex_Wall():
+    # Variables
     hex_wall = 3
     wall_length = 6 * 2.54 * 10
     wall_height = 2 * 2.54 * 10
@@ -26,25 +27,20 @@ def Hex_Wall():
     # Calculated Variables
     hex_radius = wall_height / 3
     hex_number = floor(wall_length / hex_radius)
-    segment = 360/6
-    # Calculate the missing side of the triangle
+    segment = 360 / 6
+    # Calculate the length of the flat side (along the bottom of the hexagon)
     # a^2 = b^2 + c^2 - 2bc cosA
     side_length = sqrt(pow(hex_radius,2) + pow(hex_radius,2) - (2 * hex_radius * hex_radius * cos(radians(segment))))
-    # Calculate the 'height' to the bottom of the hex
+    # Calculate the distance from the centre to the flat bottom of the hex
     # a^2 = b^2 + c^2 - rearranged
-    distance_to_side = sqrt(pow(hex_radius,2) - pow(side_length / 2,2))
-    #distance_to_side = fabs(hex_radius / cos((30/2*pi)))
+    #distance_to_side = sqrt(pow(hex_radius,2) - pow(side_length / 2,2))
+    distance_to_side = hex_radius * cos(radians(segment / 2))
+    # Calculate the X offset
     x_offset = (side_length / 2 + hex_radius)
 
-    #hex = (cylinder(r=hex_radius, h=barrier_thickness, segments=6))
-    #hex_insert = translate([0,0,-1])(cylinder(r=hex_radius - hex_wall, h=barrier_thickness + 2, segments=6))
-    #hex -= hex_insert
+    # Create the hexes
     hex = Create_Hex(radius=hex_radius, wall_height=barrier_thickness, hex_wall=hex_wall)
-    test = translate([0,0,barrier_thickness])(rotate(a=segment/2)(cube([distance_to_side,1,1],center=False)))
-    #hex += translate([hex_radius * 1.5,distance_to_side - (hex_wall / 2),0])(hex)
     hex += translate([x_offset, distance_to_side, 0])(hex)
-
-    #print(f'{wall_height} {hex_radius} ')
 
     return hex
 
